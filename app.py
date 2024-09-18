d4216f9b3d3f72be2d5da61715662e69bd193709
Neeraj Sona <112626898+nejisona@users.noreply.github.com>
2024-09-18 12:37:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+
+app = Flask(__name__)
+
+scaler = pickle.load(open("Models/scaler.pkl", 'rb'))
+model = pickle.load(open("Models/model.pkl", 'rb'))
+class_names = ['Lawyer', 'Doctor', 'Government Officer', 'Artist', 'Unknown',
+               'Software Engineer', 'Teacher', 'Business Owner', 'Scientist',
+               'Banker', 'Writer', 'Accountant', 'Designer',
+               'Construction Engineer', 'Game Developer', 'Stock Investor',
+               'Real Estate Developer']
+
+def Recommendations(gender, part_time_job, absence_days, extracurricular_activities,
+                    weekly_self_study_hours, math_score, history_score, physics_score,
+                    chemistry_score, biology_score, english_score, geography_score,
+                    total_score, average_score):
+    # Encode categorical variables
+    gender_encoded = 1 if gender.lower() == 'female' else 0
+    part_time_job_encoded = 1 if part_time_job else 0
+    extracurricular_activities_encoded = 1 if extracurricular_activities else 0
+
+    # Create feature array
+    feature_array = np.array([[gender_encoded, part_time_job_encoded, absence_days, extracurricular_activities_encoded,
+                               weekly_self_study_hours, math_score, history_score, physics_score,
+                               chemistry_score, biology_score, english_score, geography_score, total_score,
+                               average_score]])
+
+    # Scale features
+    scaled_features = scaler.transform(feature_array)
+
+    # Predict using the model
+    probabilities = model.predict_proba(scaled_features)
+
+    # Get top five predicted classes along with their probabilities
+    top_classes_idx = np.argsort(-probabilities[0])[:3]
+    top_classes_names_probs = [(class_names[idx], probabilities[0][idx]) for idx in top_classes_idx]
+
+    return top_classes_names_probs
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/recommend")
+def recomend():
+    return render_template('recommend.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
