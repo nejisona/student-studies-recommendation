86fbfb8c0c2a16a46a2af00552cbc497ed95f9f6
Neeraj Sona <112626898+nejisona@users.noreply.github.com>
2024-09-21 08:37:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 
 app = Flask(__name__)
 
+# Load the scaler, label encoder, model, and class names=====================
 scaler = pickle.load(open("Models/scaler.pkl", 'rb'))
 model = pickle.load(open("Models/model.pkl", 'rb'))
 class_names = ['Lawyer', 'Doctor', 'Government Officer', 'Artist', 'Unknown',
@@ -13,6 +14,7 @@ class_names = ['Lawyer', 'Doctor', 'Government Officer', 'Artist', 'Unknown',
                'Construction Engineer', 'Game Developer', 'Stock Investor',
                'Real Estate Developer']
 
+# Recommendations ===========================================================
 def Recommendations(gender, part_time_job, absence_days, extracurricular_activities,
                     weekly_self_study_hours, math_score, history_score, physics_score,
                     chemistry_score, biology_score, english_score, geography_score,
@@ -40,13 +42,41 @@ def Recommendations(gender, part_time_job, absence_days, extracurricular_activit
 
     return top_classes_names_probs
 
-@app.route("/")
+
+@app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route("/recommend")
-def recomend():
+@app.route('/recommend')
+def recommend():
     return render_template('recommend.html')
 
+@app.route('/pred', methods=['POST','GET'])
+def pred():
+    if request.method == 'POST':
+        gender = request.form['gender']
+        part_time_job = request.form['part_time_job'] == 'true'
+        absence_days = int(request.form['absence_days'])
+        extracurricular_activities = request.form['extracurricular_activities'] == 'true'
+        weekly_self_study_hours = int(request.form['weekly_self_study_hours'])
+        math_score = int(request.form['math_score'])
+        history_score = int(request.form['history_score'])
+        physics_score = int(request.form['physics_score'])
+        chemistry_score = int(request.form['chemistry_score'])
+        biology_score = int(request.form['biology_score'])
+        english_score = int(request.form['english_score'])
+        geography_score = int(request.form['geography_score'])
+        total_score = float(request.form['total_score'])
+        average_score = float(request.form['average_score'])
+
+        recommendations = Recommendations(gender, part_time_job, absence_days, extracurricular_activities,
+                                          weekly_self_study_hours, math_score, history_score, physics_score,
+                                          chemistry_score, biology_score, english_score, geography_score,
+                                          total_score, average_score)
+
+        return render_template('results.html', recommendations=recommendations)
+    return render_template('home.html')
+
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
